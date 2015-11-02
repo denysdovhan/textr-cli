cde9f5f6f9826f856f94af4c0f1c3499e57b46b7
Denys Dovhan <email@denysdovhan.com>
2015-11-02 23:47:48 +0200

    Move cli.js into cli/. Add some comments.

--- cli/index.js
+++ cli/index.js
@@ -7,7 +7,13 @@ import textr    from 'textr';
 import path     from 'path';
 import fs       from 'fs';
 
-
+/**
+ * CLI app instance of Meow
+ * More: https://www.npmjs.com/package/meow
+ * @param  {Object|Array|String} Text that will be write on --help or -h
+ * @param  {Object}              Minimist options (github.com/substack/minimist)
+ * @return {Object}              A Meow CLI helper object
+ */
 const cli = meow(`
   Usage
     $ textr
@@ -24,29 +30,46 @@ const cli = meow(`
   {
     default: {
       transforms: [],
-      watch: false,
-      diff: false
     },
     alias: {
       t: 'transforms',
-      w: 'watch',
-      d: 'diff'
+      h: 'help'
     }
   }
 );
 
+/**
+ * Resolve file name with current dir
+ * @param  {String} file Any relative filename
+ * @return {String}      Resolved path to file
+ */
 const cwd = file =>
   path.resolve(process.cwd(), file);
 
+/**
+ * Render text string throught textr transformer
+ * @param  {String} text Text that will be processed
+ * @param  {Array}  tfs  Array of transformers
+ * @return {Void}        Output into strout and exit
+ */
 const render = (text, tfs = []) => {
   process.stdout.write((textr().use.apply(null, tfs))(text));
   process.exit(0);
 };
 
+/**
+ * Get array of transformers.
+ * If list of transformers is a string, then split it.
+ * After all, require everything.
+ * @param  {String|Array} t String or Array that contains list of transformers
+ * @return {Array}          Return array of transform functions
+ */
 const tfs = (t = cli.flags.transforms) =>
   (typeof t === 'string' ? t.split(',') : t).map(tf => require(tf));
 
-// Get input
+/**
+ * Entry point
+ */
 if (cli.input[0]) {
   // If there is first argument, then read this file
   render(
